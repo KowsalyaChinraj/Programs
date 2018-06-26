56d6c6ceb49d511f4ed58e054a1027509503746a
KowsalyaChinraj <ckowsalya97@gmail.com>
2018-06-26 12:45:20 +0530

    star pattern

---
+++ com/basics/StarPattern.java
@@ -0,0 +1,17 @@
+package com.basics;
+
+public class StarPattern {
+	public static void main(String arhs[]) {
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+			
+			System.out.print("*");
+		}
+			System.out.print(" \n");
+			
+	}
+		
+		
+
+}
+}
