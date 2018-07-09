e27ff420ac5885a75541c198cc1853869951aa74
KowsalyaChinraj <ckowsalya97@gmail.com>
2018-07-09 11:25:09 +0530

    simple program for number pattern

---
+++ com/basics/NumberPattern.java
@@ -0,0 +1,18 @@
+package com.basics;
+
+public class NumberPattern {
+	public static void main(String arhs[]) {
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i+0;j++) {
+			
+			System.out.print(j);
+		}
+			System.out.print(" \n");
+			
+	}
+		
+		
+
+}
+
+}
